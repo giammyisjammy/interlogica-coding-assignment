cddd2d00dea2da6146425f9c5be322c9d09e662b
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2023-10-30 11:40:48 +0100

    ⚡️ Use Fastify provider in place of Express - Improves performances

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(5001);
+  // By default, Fastify listens only on the localhost 127.0.0.1 interface.
+  // If you want to accept connections on other hosts, you should specify
+  // '0.0.0.0' in the listen() call
+  // await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
